3b413cbe74c857412c620ad89ff7a68fc5e32153
Lars Schumann <LarsSchumann@mail.de>
2025-02-17 23:55:00 +0100

    day-02 part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,38 +1,80 @@
-fn part_1(input: String) {
-    let safe_count = input
+enum SafetyReport {
+    Safe,
+    FailureAt(usize),
+}
+
+fn test_safety(nums: &Vec<i32>) -> SafetyReport {
+    match nums.len() {
+        0 | 1 => SafetyReport::Safe,
+        _ => {
+            let incline = if nums[1] > nums[0] { 1 } else { -1 };
+            let mut safety_report = SafetyReport::Safe;
+            for (i, _) in nums.iter().enumerate() {
+                if i == nums.len() - 1 {
+                    break;
+                }
+                match (nums[i + 1] - nums[i]) * incline {
+                    1 | 2 | 3 => {}
+                    _ => {
+                        safety_report = SafetyReport::FailureAt(i);
+                        break;
+                    }
+                }
+            }
+            safety_report
+        }
+    }
+}
+
+fn part_1(input: &str) {
+    let safe_count: i32 = input
         .lines()
         .map(|l| {
             let nums: Vec<i32> = l.split_whitespace().map(|e| e.parse().unwrap()).collect();
-            match nums.len() {
-                0 | 1 => 1,
-                _ => {
-                    let incline = if nums[1] > nums[0] { 1 } else { -1 };
-                    let mut is_safe = 1;
-                    for (i, _) in nums.iter().enumerate() {
-                        if i == nums.len() - 1 {
-                            break;
-                        }
-                        match (nums[i + 1] - nums[i]) * incline {
-                            1 | 2 | 3 => {}
-                            _ => is_safe = 0,
-                        }
-                    }
-                    is_safe
-                }
+            match test_safety(&nums) {
+                SafetyReport::Safe => 1,
+                SafetyReport::FailureAt(_) => 0,
             }
         })
-        .sum::<i32>();
+        .sum();
     println!("{safe_count}");
 }
 
-//fn part_2(list_1: &Vec<i32>, list_2: &Vec<i32>) {
-//    todo!();
-//}
+fn part_2(input: &str) {
+    let safe_count: i32 = input
+        .lines()
+        .map(|l| {
+            let nums: Vec<i32> = l.split_whitespace().map(|e| e.parse().unwrap()).collect();
+            match test_safety(&nums) {
+                SafetyReport::Safe => 1,
+                SafetyReport::FailureAt(i) => {
+                    let mut test_1 = nums.clone();
+                    let mut test_2 = nums.clone();
+                    let mut test_3 = nums.clone();
+                    test_1.remove(i.saturating_sub(1));
+                    test_2.remove(i);
+                    test_3.remove(i.saturating_add(1));
+                    match (
+                        test_safety(&test_1),
+                        test_safety(&test_2),
+                        test_safety(&test_3),
+                    ) {
+                        (SafetyReport::Safe, _, _) => 1,
+                        (_, SafetyReport::Safe, _) => 1,
+                        (_, _, SafetyReport::Safe) => 1,
+                        _ => 0,
+                    }
+                }
+            }
+        })
+        .sum();
+    println!("{safe_count}");
+}
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt")
         .expect("input file missing or in the wrong location");
 
-    part_1(input);
-    //part_2(&list_1, &list_2);
+    part_1(&input);
+    part_2(&input);
 }
