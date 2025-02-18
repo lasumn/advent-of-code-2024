b5c83e8288b7708445ebe24162addcd879eaf73f
Lars Schumann <LarsSchumann@mail.de>
2025-02-18 06:38:24 +0100

    day-04 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,4 +1,4 @@
-fn check_word(
+fn check_word_1(
     grid: &Vec<Vec<char>>,
     word: &str,
     max_row: usize,
@@ -33,6 +33,42 @@ fn check_word(
     }
 }
 
+fn check_word_2(
+    grid: &Vec<Vec<char>>,
+    max_row: usize,
+    max_col: usize,
+    row: usize,
+    col: usize,
+) -> bool {
+    let start_row: i32 = row as i32 - 1;
+    let start_col: i32 = col as i32 - 1;
+    let end_row: i32 = row as i32 + 1;
+    let end_col: i32 = col as i32 + 1;
+
+    if 0 > start_row || end_row > max_row as i32 || 0 > start_col || end_col > max_col as i32 {
+        return false;
+    }
+
+    let mut letters_1 = vec![];
+    let mut letters_2 = vec![];
+
+    for i in -1..2 {
+        //println!("row:{row}, row_direction: {row_direction}, col:{col}, col_direction: {col_direction}, i: {i}");
+        letters_1.push(grid[(row as i32 + i) as usize][(col as i32 + i) as usize]);
+        letters_2.push(grid[(row as i32 - i) as usize][(col as i32 + i) as usize]);
+    }
+
+    if ((letters_1[0] == 'M' && letters_1[1] == 'A' && letters_1[2] == 'S')
+        || (letters_1[0] == 'S' && letters_1[1] == 'A' && letters_1[2] == 'M'))
+        && ((letters_2[0] == 'M' && letters_2[1] == 'A' && letters_2[2] == 'S')
+            || (letters_2[0] == 'S' && letters_2[1] == 'A' && letters_2[2] == 'M'))
+    {
+        true
+    } else {
+        false
+    }
+}
+
 fn part_1(input: &Vec<Vec<char>>, last_row: usize, last_col: usize) {
     let mut count = 0;
     #[rustfmt::skip]
@@ -45,7 +81,7 @@ fn part_1(input: &Vec<Vec<char>>, last_row: usize, last_col: usize) {
     for r in 0..last_row + 1 {
         for c in 0..last_col + 1 {
             for (rowd, cold) in &directions {
-                if check_word(input, "XMAS", last_row, last_col, r, c, rowd, cold) {
+                if check_word_1(input, "XMAS", last_row, last_col, r, c, rowd, cold) {
                     count += 1;
                 }
             }
@@ -55,9 +91,19 @@ fn part_1(input: &Vec<Vec<char>>, last_row: usize, last_col: usize) {
     println!("{count}");
 }
 
-//fn part_2(input: &Vec<&str>, rowc: usize, colc: usize) {
-//
-//}
+fn part_2(input: &Vec<Vec<char>>, last_row: usize, last_col: usize) {
+    let mut count = 0;
+
+    for r in 0..last_row + 1 {
+        for c in 0..last_col + 1 {
+            if check_word_2(input, last_row, last_col, r, c) {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{count}");
+}
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt")
@@ -65,5 +111,5 @@ fn main() {
     let input: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
     part_1(&input, input[0].len() - 1, input.len() - 1);
-    //part_2(&input);
+    part_2(&input, input[0].len() - 1, input.len() - 1);
 }
